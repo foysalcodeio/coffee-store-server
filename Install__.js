575d9fc957e74a11d5635e7a47f90061841a48b8
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-04-10 02:03:19 +0600

    get post done

---
+++ Install__.js
@@ -0,0 +1,11 @@
+/**
+ * 1. npm init -y
+ * 2. npm i express cors mongodb dotenv
+ * 3. now create index.js
+ * 4. go to package.json > scripts > edit - "start": "node index.js"
+ * 
+ * 
+ * 
+ * 1. install dotenv using npm
+ * 
+ */
