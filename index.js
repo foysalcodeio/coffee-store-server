45f91ee544eb0b1553d0325cf2035cd8e1ee4198
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-04-25 12:12:39 +0600

    done server

--- index.js
+++ index.js
@@ -68,15 +68,32 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await coffeeCollection.findOne(query);
-      res.send(result)
+      res.send(result);
     })
 
+    // 4. 2. UPDATE
 
-
-
+    app.put('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true};
+      const updatedCoffee = req.body;
+      const coffee = {
+        $set:{
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, options )
+      res.send(result);
+    })
 
     
-
 // ============================== CRUD ACTION END =======================
 
 
